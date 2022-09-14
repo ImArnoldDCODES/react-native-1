e6221cfd14e9707c4f85f5d91888d46313ae2726
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-09-14 13:18:14 -0700

    status bar

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, Alert, Button } from 'react-native';
+import { StyleSheet, SafeAreaView, Alert, Button, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, containerStyle]}>
       <Button
         color="orange"
         title="Click Me" onPress={() => Alert.prompt("My Title", "My message", text => console.log(text))} />
@@ -11,12 +10,13 @@ export default function App() {
   );
 }
 
+const containerStyle = { backgroundColor: "orange" }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
 
