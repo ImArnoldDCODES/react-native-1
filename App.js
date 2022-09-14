fcb7195df9c5fa1bdc98e94e6abcffcb7cedbda2
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-09-14 12:59:52 -0700

    alerts

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight } from 'react-native';
+import { StyleSheet, SafeAreaView, Alert, Button } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello react native</Text>
-      <TouchableHighlight onPress={() => console.log('img tap')}>
-        <Image
-          style={styles.logo}
-          fadeDuration={1000}
-          source={{
-            uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-          }}
-        />
-      </TouchableHighlight>
-      {/* <Image
-        style={styles.tinyLogo}
-        source={require('./assets/icon.png')} /> */}
+      <Button
+        color="orange"
+        title="Click Me" onPress={() => Alert.prompt("My Title", "My message", text => console.log(text))} />
     </SafeAreaView>
   );
 }
@@ -27,14 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  logo: {
-    width: 100,
-    height: 100,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
   },
 });
 
